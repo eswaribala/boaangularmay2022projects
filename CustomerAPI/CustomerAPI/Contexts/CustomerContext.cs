e39f046da99652d4388ff6d8def7b51c51dde4f3
eswaribala <parameswaribala@gmail.com>
2022-05-09 17:24:40 +0530

    initial commit

---
+++ CustomerAPI/CustomerAPI/Contexts/CustomerContext.cs
@@ -0,0 +1,25 @@
+﻿using CustomerAPI.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+
+namespace CustomerAPI.Contexts
+{
+    public class CustomerContext:DbContext
+    {
+
+        public CustomerContext(DbContextOptions<CustomerContext> options) 
+            : base(options)
+        {
+            this.Database.EnsureCreated();
+        }
+
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Address> Addresses { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Address>().HasOne<Customer>();
+
+        }
+    }
+}
