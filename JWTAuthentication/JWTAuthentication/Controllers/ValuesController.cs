f1a5e88f4d69a8baf1323865db87073ba1c8d98e
eswaribala <parameswaribala@gmail.com>
2022-05-10 17:06:07 +0530

    initial commit

---
+++ JWTAuthentication/JWTAuthentication/Controllers/ValuesController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace JWTAuthentication.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : Controller
+    {
+        // GET api/values
+        [HttpGet]
+        [Authorize]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            var currentUser = HttpContext.User;
+            int spendingTimeWithCompany = 0;
+            String userName = null;
+
+            if (currentUser.HasClaim(c => c.Type == "UserName"))
+            {
+                userName = currentUser.Claims.FirstOrDefault(c => c.Type == "UserName").Value;          }
+
+            if (currentUser.HasClaim(c => c.Type == "DateOfJoining"))
+            {
+                DateTime date = DateTime.Parse(currentUser.Claims.FirstOrDefault(c => c.Type == "DateOfJoining").Value);
+                spendingTimeWithCompany = DateTime.Today.Year - date.Year;
+            }
+
+            if (spendingTimeWithCompany > 5)
+            {
+                return new string[] { "High Time1", "High Time2", "High Time3", "High Time4", "High Time5" };
+            }
+            else
+            {
+                return new string[] { "value1", "value2", "value3", "value4", "value5" };
+            }
+        }
+
+    }
+}
