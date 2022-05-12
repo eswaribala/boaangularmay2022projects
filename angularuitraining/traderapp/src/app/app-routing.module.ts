bcaf988d2dcf5f7a918bf4f0a1fdcf44a35bd37c
eswaribala <parameswaribala@gmail.com>
2022-05-12 11:41:30 +0530

    initial commit

--- angularuitraining/traderapp/src/app/app-routing.module.ts
+++ angularuitraining/traderapp/src/app/app-routing.module.ts
@@ -1,7 +1,74 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {StaticdataComponent} from "./staticdata/staticdata.component";
+import {CustomerComponent} from "./staticdata/customer/customer.component";
+import {CurrencyComponent} from "./staticdata/currency/currency.component";
+import {CalendarComponent} from "./staticdata/calendar/calendar.component";
+import {BankComponent} from "./staticdata/bank/bank.component";
+import {MarketdataComponent} from "./marketdata/marketdata.component";
+import {LiborComponent} from "./marketdata/libor/libor.component";
+import {ForexComponent} from "./marketdata/forex/forex.component";
+import {TradebookingComponent} from "./tradebooking/tradebooking.component";
+import {EconomicComponent} from "./tradebooking/economic/economic.component";
+import {NoneconomicComponent} from "./tradebooking/noneconomic/noneconomic.component";
+import {LogoutComponent} from "./logout/logout.component";
 
-const routes: Routes = [];
+const routes: Routes = [{
+   path:'StaticData',
+   component:StaticdataComponent,
+   children:[{
+     path:'Customer',
+     component: CustomerComponent
+   },
+     {
+       path:'Currency',
+       component: CurrencyComponent
+     },
+     {
+       path:'Calendar',
+       component: CalendarComponent
+     },
+     {
+       path:'Bank',
+       component: BankComponent
+     }
+   ]
+},
+  {
+    path:'MarketData',
+    component:MarketdataComponent,
+    children:[{
+      path:'LIBOR',
+      component: LiborComponent
+    },
+      {
+        path:'FOREX',
+        component: ForexComponent
+      }
+    ]
+  },
+  {
+    path:'TradeBooking',
+    component:TradebookingComponent,
+    children:[{
+      path:'Economic',
+      component: EconomicComponent
+    },
+      {
+        path:'NonEconomic',
+        component: NoneconomicComponent
+      }
+    ]
+  },
+  {
+    path:'Logout',
+    component:LogoutComponent
+
+  }
+
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- angularuitraining/traderapp/src/app/models/menudata.ts
+++ angularuitraining/traderapp/src/app/models/menudata.ts
@@ -1,20 +1,20 @@
 import {Item, SubItem} from "./menuitem";
 
 export const menuData:Item[]=[
-  new Item('Static Data','pi pi-fw pi-plus', '',
-    [new SubItem('Customer','pi pi-fw pi-arrow-right',''),
-      new SubItem('Currency','pi pi-fw pi-arrow-right',''),
-      new SubItem('Calendar','pi pi-fw pi-arrow-right',''),
-      new SubItem('Bank','pi pi-fw pi-arrow-right','')
+  new Item('Static Data','pi pi-fw pi-plus', 'StaticData',
+    [new SubItem('Customer','pi pi-fw pi-arrow-right','StaticData/Customer'),
+      new SubItem('Currency','pi pi-fw pi-arrow-right','StaticData/Currency'),
+      new SubItem('Calendar','pi pi-fw pi-arrow-right','StaticData/Calendar'),
+      new SubItem('Bank','pi pi-fw pi-arrow-right','StaticData/Bank')
     ]),
-  new Item('Market Data','pi pi-fw pi-plus', '',
-    [new SubItem('LIBOR','pi pi-fw pi-arrow-right',''),
-      new SubItem('FOREX','pi pi-fw pi-arrow-right','')
+  new Item('Market Data','pi pi-fw pi-plus', 'MarketData',
+    [new SubItem('LIBOR','pi pi-fw pi-arrow-right','MarketData/LIBOR'),
+      new SubItem('FOREX','pi pi-fw pi-arrow-right','MarketData/FOREX')
     ]),
-  new Item('Trade Booking','pi pi-fw pi-plus', '',
-    [new SubItem('Economic Trading','pi pi-fw pi-arrow-right',''),
-      new SubItem('Non Economic Trading','pi pi-fw pi-arrow-right','')
+  new Item('Trade Booking','pi pi-fw pi-plus', 'TradeBooking',
+    [new SubItem('Economic Trading','pi pi-fw pi-arrow-right','TradeBooking/Economic'),
+      new SubItem('Non Economic Trading','pi pi-fw pi-arrow-right','TradeBooking/NonEconomic')
     ]),
-  new Item('Exit/Logout','pi pi-fw pi-plus', '',
+  new Item('Exit/Logout','pi pi-fw pi-plus', 'Logout',
     []),
 ];
